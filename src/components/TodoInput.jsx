94f54b91bcca82c9521bcb4a23c401f40e05f7c5
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-08-08 12:15:13 +0100

    feat: Add TodoInput and Todos components
    This commit adds the TodoInput and Todos components to the project. The TodoInput component is responsible for rendering an input field and an icon for adding new tasks. The Todos component is a placeholder component that will display the list of tasks.
    
    The changes include:
    - Importing the LuCircle icon from the react-icons/lu package in TodoInput.jsx
    - Updating the JSX code in TodoInput.jsx to include the input field and icon
    - Importing the Todos component in TodoList.jsx
    - Adding the Todos component to the JSX code in TodoList.jsx
    - Creating a new file, Todos.jsx, which contains the Todos component
    
    These changes are necessary to implement the basic functionality of adding and displaying tasks in the TodoList component.

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,9 +1,16 @@
-
+import { LuCircle } from "react-icons/lu";
 
 const TodoInput = () => {
   return (
-    <div>TodoInput</div>
-  )
-}
+    <div className="bg-white rounded-lg gap-2 flex items-center justify-around px-2 w-full">
+      <LuCircle className="text-2xl text-gray-300" />
+      <input
+        className="w-full h-10 pl-2"
+        type="text"
+        placeholder="Add a new task..."
+      />
+    </div>
+  );
+};
 
-export default TodoInput
+export default TodoInput;
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,10 +1,12 @@
 import TodoInput from "./TodoInput";
+import Todos from "./Todos";
 
 const TodoList = () => {
   return (
     <div>
       <h1>TodoList</h1>
       <TodoInput />
+      <Todos />
     </div>
   );
 };
